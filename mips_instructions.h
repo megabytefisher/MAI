48291c2d56ab185639baee65cca632686ceb5516
Edward Prins <prinsiii@memphis.edu>
2015-03-19 18:00:41 -0500

    Add sub, subi, or, ori, and xor

--- mips_instructions.h
+++ mips_instructions.h
@@ -9,8 +9,16 @@ typedef void (*instruction_function)(mips_state*, char*);
 // instruction implementations
 void add(mips_state*, char*);
 void addi(mips_state*, char*);
+void and(mips_state*, char*);
+void andi(mips_state*, char*);
+void sub(mips_state*, char*);
+void subi(mips_state*, char*);
+void or(mips_state*, char*);
+void ori(mips_state*, char*);
+void xor(mips_state*, char*);
+
 
 // pseudo-dictionary for going from char* -> function pointer
 extern const char* INSTRUCTION_NAMES[];
 extern const instruction_function INSTRUCTION_IMPLEMENTATION[];
-extern const int INSTRUCTION_COUNT;
+extern const int INSTRUCTION_COUNT,NT;
