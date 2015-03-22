82b0b52c4693a1eaf116dd7ce8e40e493eaedc0c
Kevin Fisher <megabytefisher@gmail.com>
2015-03-21 20:48:15 -0500

    idk how to merge
    fixed merge crap

--- mips_instructions.h
+++ mips_instructions.h
@@ -1,23 +1,3 @@
-<<<<<<< HEAD
-#include "mips_memory.h"
-#include "utils.h"
-
-#pragma once
-
-// function pointer to a mips instruction implementation
-typedef void (*instruction_function)(mips_state*, char*);
-
-// instruction implementations
-void add(mips_state*, char*);
-void addi(mips_state*, char*);
-void and(mips_state*, char*);
-void andi(mips_state*, char*);
-
-// pseudo-dictionary for going from char* -> function pointer
-extern const char* INSTRUCTION_NAMES[];
-extern const instruction_function INSTRUCTION_IMPLEMENTATION[];
-extern const int INSTRUCTION_COUNT;
-=======
 #include "mips_memory.h"
 #include "utils.h"
 
@@ -44,4 +24,4 @@ void slr(mips_state*, char*);
 extern const char* INSTRUCTION_NAMES[];
 extern const instruction_function INSTRUCTION_IMPLEMENTATION[];
 extern const int INSTRUCTION_COUNT,NT;
->>>>>>> 96e9a4790ba03a6c5f89c0eee0d38ea18f577cd0
+
