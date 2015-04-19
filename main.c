c7fb2342ed850c9bdfe757e818c14c415efc977a
Kevsprk <megabytefisher@gmail.com>
2015-04-19 18:26:14 -0500

    clean up
    mostly reverting changes

--- main.c
+++ main.c
@@ -14,47 +14,17 @@ int main(int argc, char** argv) {
     
     if (argc == 1) // no parameters. run in live interpreter mode.
     {
-        // While loop will continue until the user enter ".data".
-        while (1) {
-            printf("Welcome to MAI (MIPS Assembly Interpreter).\n");
-            printf("Enter commands to execute and press enter to execute. For help, execute 'help'.\n");
+        printf("Welcome to MAI (MIPS Assembly Interpreter).\n");
+        printf("Enter commands to execute and press enter to execute. For help, execute 'help'.\n");
 
-            printf(">> ");
-            char inputLine[56];
-            char FLAG[56] = ".data";
-            char* p;
+        // read next input line
+        printf(">> ");
+        char inputLine[56];
+        char* p;
 
-            // read next input line
-            p = fgets(inputLine, sizeof(inputLine), stdin);
-            // Used to insure user input .data
-            // printf("%d",strcmp(inputLine,FLAG));
-            if (strcmp(inputLine,FLAG) == 10) {
-                printf(".data\n");
-                while (1)
-                {
-                    printf(">> ");
-                    char inputLine[56];
-                    char* p;
-
-                    // read next input line
-                    p = fgets(inputLine, sizeof(inputLine), stdin);
-                    
-                    if (strcmp(inputLine, FLAG) == 16) {
-                        printf(".text\n");
-                        while (1)
-                        {
-                            printf(">> ");
-                            char inputLine[56];
-                            char* p;
-
-                            // read next input line
-                            p = fgets(inputLine, sizeof(inputLine), stdin);
-                            run_from_string(&registers, inputLine);
-                        }
-                    }
-                }
-            }
-        }
+        // read next input line
+        p = fgets(inputLine, sizeof(inputLine), stdin);
+        run_from_string(&registers, inputLine);
     }
     
     return (EXIT_SUCCESS);
--- nbproject/Makefile-variables.mk
+++ nbproject/Makefile-variables.mk
@@ -7,21 +7,21 @@ CND_BASEDIR=`pwd`
 CND_BUILDDIR=build
 CND_DISTDIR=dist
 # Debug configuration
-CND_PLATFORM_Debug=Cygwin_4.x-Windows
-CND_ARTIFACT_DIR_Debug=dist/Debug/Cygwin_4.x-Windows
+CND_PLATFORM_Debug=GNU-MacOSX
+CND_ARTIFACT_DIR_Debug=dist/Debug/GNU-MacOSX
 CND_ARTIFACT_NAME_Debug=mai
-CND_ARTIFACT_PATH_Debug=dist/Debug/Cygwin_4.x-Windows/mai
-CND_PACKAGE_DIR_Debug=dist/Debug/Cygwin_4.x-Windows/package
+CND_ARTIFACT_PATH_Debug=dist/Debug/GNU-MacOSX/mai
+CND_PACKAGE_DIR_Debug=dist/Debug/GNU-MacOSX/package
 CND_PACKAGE_NAME_Debug=mai.tar
-CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin_4.x-Windows/package/mai.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/GNU-MacOSX/package/mai.tar
 # Release configuration
-CND_PLATFORM_Release=Cygwin_4.x-Windows
-CND_ARTIFACT_DIR_Release=dist/Release/Cygwin_4.x-Windows
+CND_PLATFORM_Release=GNU-MacOSX
+CND_ARTIFACT_DIR_Release=dist/Release/GNU-MacOSX
 CND_ARTIFACT_NAME_Release=mai
-CND_ARTIFACT_PATH_Release=dist/Release/Cygwin_4.x-Windows/mai
-CND_PACKAGE_DIR_Release=dist/Release/Cygwin_4.x-Windows/package
+CND_ARTIFACT_PATH_Release=dist/Release/GNU-MacOSX/mai
+CND_PACKAGE_DIR_Release=dist/Release/GNU-MacOSX/package
 CND_PACKAGE_NAME_Release=mai.tar
-CND_PACKAGE_PATH_Release=dist/Release/Cygwin_4.x-Windows/package/mai.tar
+CND_PACKAGE_PATH_Release=dist/Release/GNU-MacOSX/package/mai.tar
 #
 # include compiler specific variables
 #
